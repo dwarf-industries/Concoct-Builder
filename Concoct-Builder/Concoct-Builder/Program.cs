b2ea635929be5a50f26d8e214874805dcf2a2bd3
Kristifor Milchev <kristiformilchev615@gmail.com>
2021-08-28 21:57:30 +0300

    Implementing Grid View Component

--- Concoct-Builder/Concoct-Builder/Program.cs
+++ Concoct-Builder/Concoct-Builder/Program.cs
@@ -4,13 +4,34 @@ namespace Concoct_Builder
     using ElectronNET.API;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.Extensions.Hosting;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
     public class Program
     {
+        private static List<string> ActiveUniqueIds { get; set; }
+
         public static void Main(string[] args)
         {
+            ActiveUniqueIds = new List<string>();
             CreateHostBuilder(args).Build().Run();
         }
+
+
+        public static string GetUniqueId()
+        {
+            var chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+            var random = new System.Random();
+            var uniqueName = $"{Guid.NewGuid().ToString().Split('-').FirstOrDefault()}{chars[random.Next(chars.Length - 1)]}{chars[random.Next(chars.Length + 1)]}{chars[random.Next(chars.Length - 2)]}";
+            if (ActiveUniqueIds.Any(x => x == uniqueName))
+                ActiveUniqueIds.Add($"{uniqueName}{chars[random.Next(chars.Length - 1)]}");
+            else
+                ActiveUniqueIds.Add(uniqueName);
+
+            return uniqueName;
+        }
+
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
          Host.CreateDefaultBuilder(args)
--- Concoct-Builder/Concoct-Builder/Startup.cs
+++ Concoct-Builder/Concoct-Builder/Startup.cs
@@ -33,9 +33,11 @@ namespace Concoct_Builder
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseDeveloperExceptionPage();
+
+                // app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseHsts();
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
