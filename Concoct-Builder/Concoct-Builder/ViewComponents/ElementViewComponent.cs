66ae484e3b076aea2dee8d2b88a389d49242127f
Kristifor Milchev <kristiformilchev615@gmail.com>
2021-08-29 20:10:08 +0300

    Implementing the base constructor for the draggable, parent container that will be used to layout elements on the screen and initialize viewcomponents to construct screen layouts.

---
+++ Concoct-Builder/Concoct-Builder/ViewComponents/ElementViewComponent.cs
@@ -0,0 +1,14 @@
+﻿namespace Concoct_Builder.ViewComponents
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ElementViewComponent : ViewComponent
+    {
+
+        public IViewComponentResult Invoke(string componentName)
+        {
+            ViewData["RenderComponent"] = componentName;
+            return View();
+        }
+    }
+}
