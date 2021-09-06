0d62327531e30b41cff07bc118ad789c93c59cf9
Kristifor Milchev <kristiformilchev615@gmail.com>
2021-09-06 20:46:11 +0300

    Implementing the bease for the placeholder widget.

---
+++ Concoct-Builder/Concoct-Builder/ViewComponents/PlaceholderViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Concoct_Builder.ViewComponents
+{
+    public class PlaceholderViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
