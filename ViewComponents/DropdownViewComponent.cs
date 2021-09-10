01fe1b6f28be58838a98576b6a9d0483711cb9c9
Kristifor Milchev <kristiformilchev615@gmail.com>
2021-09-10 19:31:56 +0300

    Implementing Accordion Widget, TimePicker, Multiselect,MenuBar, ListBox, InPlaceEditor,Dropdown

---
+++ ViewComponents/DropdownViewComponent.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace Concoct_Builder.ViewComponents 
+{
+    public class DropdownViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ ViewComponents/TimePickerViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Concoct_Builder.ViewComponents
+{
+    public class TimePickerViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
