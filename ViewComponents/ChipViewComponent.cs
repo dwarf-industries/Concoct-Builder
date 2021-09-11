ff1dbc217bbf2dbd442a50fcf491f62e786369b0
Kristifor Milchev <kristiformilchev615@gmail.com>
2021-09-11 15:50:56 +0300

    Implementing the following widgets, badge, color picker, file uploader, file explorer, input, tab and toast

--- ViewComponents/ChipViewComponent.cs
+++ ViewComponents/ChipViewComponent.cs
@@ -4,7 +4,6 @@
 
     public class ChipViewComponent : ViewComponent
     {
-
         public IViewComponentResult Invoke()
         {
             return View();
---
+++ ViewComponents/FileUploaderViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Concoct_Builder.ViewComponents
+{
+    public class FileUploaderViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
