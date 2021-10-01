c3401293a11ef133949bfb171801720fe0a73f0d
Kristifor Milchev <kristiformilchev615@gmail.com>
2021-10-01 21:14:45 +0300

    Moving the layout designer to a separate layout and setting it behind dashborad controller. Implementing the base UI for authenication with concoct cloud instances.

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,35 @@
+﻿using Concoct_Builder.Datalayer;
+using Concoct_Builder.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Concoct_Builder.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly FileHandler handler;
+
+        public DashboardController(ILogger<HomeController> logger, IFileHandler fileHandler)
+        {
+            _logger = logger;
+            handler = (FileHandler)fileHandler;
+        }
+        public IActionResult Index()
+        {
+            ViewData["Organizations"] = handler.GetAllOrganizations();
+            ViewData["Components"] = GetWorkItemComponents();
+            return View();
+        }
+
+        private List<Widget> GetWorkItemComponents()
+        {
+            return handler.GetAllWidgets();
+        }
+
+    }
+}
